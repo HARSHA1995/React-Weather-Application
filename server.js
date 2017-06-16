36ad1a2d13b3cbc50175b045d6919aa58f1d3fae
Harsha <harsha@mvpcl210-67-1.af.uc.edu>
2017-06-16 15:38:54 -0400

    change

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3000; //For heroku to start
 //our weather map api works on http but not HTTPS. so our application for free version
 // we gonna redirect https traffic to http.
 app.use(function(req,res,next){  //Express middleware
-  if(req.headers['x-forwarded -proto'] === 'http'){
+  if(req.headers['x-forwarded-proto'] === 'http'){
     next();
   }else{
     res.redirect('http://' + req.hostname + req.url);
