c82654e354cf05e2d12f7a14550989887062171f
Harsha <harsha@Harshas-MacBook-Pro.local>
2017-06-16 20:35:35 -0400

    http/https

--- server.js
+++ server.js
@@ -4,13 +4,13 @@ var express = require('express');
 var app = express();
 const PORT = process.env.PORT || 3000;
 
-// our weather map api works on http but not HTTPS. so our application For free version we gonna redirect https traffic to http.
+// our weather map api free plan works on http but not HTTPS. so our application For free version we gonna redirect https traffic to http.
 //Express middleware (app.use(function (req, res, next))
 app.use(function (req, res, next){
-  if (req.headers['x-forwarded-proto'] === 'http') {
-    next();
-  } else {
+  if (req.headers['x-forwarded-proto'] === 'https') {
     res.redirect('http://' + req.hostname + req.url);
+  } else {
+    next();
   }
 });
 
