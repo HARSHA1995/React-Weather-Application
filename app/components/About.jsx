4efc2cec5ef9f97fc58afc265a7d3926fef5227e
Harsha <harsha@Harshas-MacBook-Pro.local>
2017-06-17 18:15:35 -0400

    Foundation Framework to About Page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -23,9 +23,20 @@ var React = require('react');
 var About = (props) => {
    return(
          <div>
-           <h1> About </h1>
-           <p> Welcome to About page </p>
+           <h1 className = "text-center"> About </h1>
+           <p> This is a Weather Application build on React and Foundation Framework. </p>
+           <p>Here are some of the tools I used:</p>
+           <ul>
+             <li>
+               <a href="https://facebook.github.iO/react">React</a> = This was the JavaScript Framework used.
+             </li>
+             <li>
+               <a href="http://openweathermap.org">Open Weather Map</a> = I used Open Weather Map to search for weather data by city name.
+             </li>
+           </ul>
          </div>
        )
 };
+
+
 module.exports = About;
